09b5978b4d9eec054a75c21741f9109b051c7fa6
Vind1Viet <vindiviet@gmail.com>
2024-06-29 17:17:54 +0700

    still in development and not sure it will be beautiful and bug free and

--- src/src/components/navbar/Navbar.js
+++ src/src/components/navbar/Navbar.js
@@ -1,27 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import "./Navbar.css"
 import logo from '../assets/logo.png'
-import menu_bar from '../assets/menu-bar.png'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Navbar= () => {
   const [menu,setMenu] = useState("home");
+
+  const navRef =useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  }
+
   return (
     <div className="navbar">
       <div className='nav-logo'>
         <img src={logo} alt=""/>
         <p>JustaTea</p>
       </div>
-      <ul className='nav-menu'>
-        <li onClick={()=> {setMenu("home")}}><Link style={{textDecoration: 'none'}} to='/'>Trang chủ</Link>{menu==="home"? <hr/>:<></>}</li>
-        <li onClick={()=> {setMenu("product")}}><Link style={{textDecoration: 'none'}} to ='/Product'>Thực đơn</Link>{menu==="product"? <hr/>:<></>}</li>
-        <li onClick={()=> {setMenu("about")}}><Link style={{textDecoration: 'none'}} to ='/About'>Về chúng tôi</Link>{menu==="about"? <hr/>:<></>}</li>
-        <li onClick={()=> {setMenu("picture")}}><Link style={{textDecoration: 'none'}} to ='/Picture'>Hình ảnh</Link>{menu==="picture"? <hr/>:<></>}</li>
-        <li onClick={()=> {setMenu("contact")}}><Link style={{textDecoration: 'none'}} to ='/Contact'>Liên hệ</Link>{menu==="contact"? <hr/>:<></>}</li>
-      </ul>
-      <div className='nav-login-button'>
-        <Link to ='/LoginSignup'><button onClick={()=> {setMenu("login")}}>Đăng nhập</button></Link>
-      </div>
+      <nav /*className={`nav-menu ${isOpen ? 'open' : ''}`} */ className="nav-menu" ref={navRef}>
+        <a onClick={()=> {setMenu("home"); showNavbar();} }><Link style={{textDecoration: 'none'}} to ='/'>Trang chủ</Link>{menu==="home"? <hr/>:<></>}</a>
+        <a onClick={()=> {setMenu("product"); showNavbar();}}><Link style={{textDecoration: 'none'}} to ='/Product'>Thực đơn</Link>{menu==="product"? <hr/>:<></>}</a>
+        <a onClick={()=> {setMenu("about"); showNavbar();}}><Link style={{textDecoration: 'none'}} to ='/About'>Về chúng tôi</Link>{menu==="about"? <hr/>:<></>}</a>
+        <a onClick={()=> {setMenu("picture"); showNavbar();}}><Link style={{textDecoration: 'none'}} to ='/Picture'>Hình ảnh</Link>{menu==="picture"? <hr/>:<></>}</a>
+        <a onClick={()=> {setMenu("contact"); showNavbar();}}><Link style={{textDecoration: 'none'}} to ='/Contact'>Liên hệ</Link>{menu==="contact"? <hr/>:<></>}</a>
+        <div className='nav-login-button'>
+          <Link to ='/LoginSignup' style={{textDecoration: 'none'}}><button onClick={()=> {setMenu("login"); showNavbar();}}>Đăng nhập</button></Link>
+        </div>
+      </nav>
+      <span className="navbar-toggle-icon" onClick={showNavbar}>&#9776;</span>
     </div>
   )
 }
