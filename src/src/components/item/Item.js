dba5832866c4e2ff6d7f8cd1c462af826cd403f4
Vind1Viet <vindiviet@gmail.com>
2024-07-30 10:09:25 +0700

    Fix menu

--- src/src/components/item/Item.js
+++ src/src/components/item/Item.js
@@ -2,28 +2,27 @@ import React from 'react'
 import './Item.css'
 import {Link} from 'react-router-dom'
 
-// const Item = (props) => {
-//   return (
-//     <Link style={{textDecoration: 'none'}} to ={`/Menu/${props.id}`}>
-//     <div className='item'>
-//         <img src={props.imageUrl} alt = ""/>
-//         <div className='item-info'>
-//             <h>{props.name}</h>
-//             <p>{props.price}đ</p>
-//         </div>
-//     </div> 
-//     </Link>
-//   )
-// }
-
 const Item = ({ id, name, imageUrl, price, tag }) => {
   return (
+    <Link style={{textDecoration: 'none'}} to ={`/Menu/${id}`}>
     <div className='item'>
-      <img src={imageUrl} alt={name} className='item-image' />
-      <h3>{name}</h3>
-      <p>{price.toLocaleString()}đ</p>
-      <p>{tag}</p>
-    </div>
-  );
+        <img src={imageUrl} alt = ""/>
+        <div className='item-info'>
+            <h>{name}</h>
+            <p>{price}đ</p>
+        </div>
+    </div> 
+    </Link>
+  )
 }
+
+// const Item = ({ id, name, imageUrl, price, tag }) => {
+//   return (
+//     <div className='item'>
+//       <img src={imageUrl} alt={name} className='item-image' />
+//       <h3>{name}</h3>
+//       <p>{price.toLocaleString()}đ</p>
+//     </div>
+//   );
+// }
 export default Item
