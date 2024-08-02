bbd48d810b2f5b2d1bc78577384c27fa45d25bad
Vind1Viet <vindiviet@gmail.com>
2024-08-02 23:37:09 +0700

    First cart UI design

---
+++ src/src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/Cart.css'
+import CartItem from '../components/cartitem/CartItem'
+
+const Cart = () => {
+  return (
+    <CartItem/>
+  )
+}
+
+export default Cart
