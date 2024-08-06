0510843853b00a1ce0aa115e83b669858c7bf770
trandoanhuyphuoc19112004 <tdhphuoc22clc@fitus.edu.vn>
2024-08-06 11:28:48 +0700

    update cart & voucher

---
+++ src/src/model/Cart.js
@@ -0,0 +1,10 @@
+class CustomerCart {
+    constructor() {
+      this.cartList = [];
+      this.quantityList = [];
+      this.sizeList = [];
+      this.toppingList = [];
+      this.priceList = [];
+    }
+}
+export default Cart
