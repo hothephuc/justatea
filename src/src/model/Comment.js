dd1b9fad98ad968493d1db456f1685401ed91f38
binhtran <tranducbinh2004@gmail.com>
2024-08-06 09:54:13 +0700

    class for comment and product

---
+++ src/src/model/Comment.js
@@ -0,0 +1,10 @@
+class Comment{
+    constructor(userID, productID, text, dateCreated){
+        this.userID = userID;
+        this.productID = productID;
+        this.text = text;
+        this.dateCreated = dateCreated;
+    }
+}
+
+export default Comment
---
+++ src/src/model/Product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(name, tag, description, imageUrl, price) {
+        this.name = name;
+        this.tag = tag;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+}
+
+// Exporting the Product class
+export default Product;
