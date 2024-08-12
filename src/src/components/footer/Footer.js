d202cc8eee3e3227d98607592de890d0c4350aec
Vind1Viet <vindiviet@gmail.com>
2024-08-13 01:08:47 +0700

    big update 1.0

--- src/src/components/footer/Footer.js
+++ src/src/components/footer/Footer.js
@@ -12,12 +12,19 @@ const Footer = () => {
       </div>
       <hr />
       <div className='footer-content'>
+        <div className='footer-about'>
+          <h1>Về chúng tôi</h1>
+          <p>Trang web chính thức của quán cà phê JustaTea, thiết kế bởi JustaTea Team,
+             nơi bạn có thể khám phá và thưởng thức các loại đồ uống thơm ngon nhất.</p>
+        </div>
         <div className='footer-contact'>
+          <h1>Liên hệ</h1>
           <p>Địa chỉ: 227 Đ. Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh</p>
           <p>Điện thoại: 0901931656</p>
           <p>Email: TranBinhPhuocViet@gmail.com</p>
         </div>
         <div className='footer-social'>
+          <h1>Mạng xã hội</h1>
           <a href="https://www.facebook.com/dean.micheal.988" target="_blank" rel="noopener noreferrer">
             <FaFacebook /> https://www.facebook.com/dean.micheal.988
           </a>
